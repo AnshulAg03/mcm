9c9c45a98b26e855267c0ea599a2b0fc53b23544
Anshul Agarwal <anshulagarwal0391@gmail.com>
2020-10-13 01:24:23 +0530

    Dashboard, teamDisable API

--- src/main/java/com/mcm/api/entity/CaseTeamId.java
+++ src/main/java/com/mcm/api/entity/CaseTeamId.java
@@ -8,6 +8,8 @@ import javax.persistence.Embeddable;
 @Embeddable
 public class CaseTeamId implements Serializable{
 
+	private static final long serialVersionUID = 1L;
+
 	@Column(name="TID")
 	private String team;
 	
